a88a98eb23c39e0f9de43c6cdccde2f326e4de40
Makidonskiu <www.salamandr@mail.ru>
2022-05-08 05:07:10 +0300

    Sort and imagePizza

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Categories = ({items}) => {
+export const Categories = ({ items, onClick }) => {
+  const [active, setActive] = useState(null);
+
+  const onSelectItem = (index) => {
+    setActive(index);
+  };
+
+  console.log();
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((item, index) => <li key = {`${index} text`}>{item}</li>)}
+        <li onClick={() => onSelectItem(null)} className={active === null ? 'active' : ''}>
+          Все
+        </li>
+        {items.map((item, index) => (
+          <li
+            className={active === index ? 'active' : ''}
+            onClick={() => onSelectItem(index)}
+            key={`${item}_${index}`}>
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
